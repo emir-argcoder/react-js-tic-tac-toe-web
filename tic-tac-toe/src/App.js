1bf56ed8f8833865dec7fbffd8c882c097a75bc6
emir-argcoder <ea.samsam@gmail.com>
2024-10-11 23:09:53 -0300

    Initial commit with visible game board

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { useState } from 'react';
+import Board from './components/Board/Board';
+
+const App = () => {
+  const [turn, setTurn] = useState('X');
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [score, setScore] = useState({ 
+    X: 0,
+    O: 0,
+  });
+  
+  const checkForWinner = squares => {
+    setTurn(turn === 'X' ? 'O' : 'X');
+  }
+  
+  const handleClick = square => {
+    let newSquares = [...squares]; 
+    newSquares.splice(square, 1, turn); 
+    setSquares(newSquares);
+    checkForWinner(newSquares);
+  }
+  
+  return (
+    <div className="container">
+      <Board squares={squares} onClick={handleClick}/>
+    </div>
+  );
+}
+
+export default App;
